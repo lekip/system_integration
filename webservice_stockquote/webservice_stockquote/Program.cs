b4f0ece516d8eba18b9b9227be6e16ed8eadd5e4
lekip <kasp964n@edu.eal.dk>
2017-02-06 14:25:34 +0100

    stock service

--- webservice_stockquote/webservice_stockquote/Program.cs
+++ webservice_stockquote/webservice_stockquote/Program.cs
@@ -10,6 +10,22 @@ namespace webservice_stockquote
     {
         static void Main(string[] args)
         {
+            String k = "";
+            while (k != "2")
+            {
+                Console.Clear();
+                Console.WriteLine("Menu\n\n");
+                Console.WriteLine("1) Læs kurs for BP");
+                Console.WriteLine("2) Afslut");
+                k = Console.ReadKey().KeyChar.ToString();
+                Console.Clear();
+                if (k == "1")
+                {
+                    stock.StockQuoteSoapClient client = new stock.StockQuoteSoapClient("StockQuoteSoap");
+                    Console.WriteLine(client.GetQuote("BP"));
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
